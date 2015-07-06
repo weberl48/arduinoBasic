aa199ee21daba70c6452a35f6af3807ecfda8ff2
Lucas Weber <weberl48@outlook.com>
2015-07-06 09:21:37 -0600

    added user input

--- routes/index.js
+++ routes/index.js
@@ -3,54 +3,84 @@ var router = express.Router();
 var weather = require("../weather");
 var unirest = require("unirest");
 var request = require('request');
+var validate = require('../lib/javascript/logic.js');
+var timeInput = require('../lib/javascript/indexLogic.js');
 
 var nodemailer = require("nodemailer");
-  var sendgrid  = require('sendgrid')(process.env.api_user, process.env.api_key);
+var sendgrid = require('sendgrid')(process.env.api_user, process.env.api_key);
 var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/weather');
 
 var weatherCollection = db.get('weather');
 /* GET home page. */
 router.get('/data', function(req, res, next) {
-  weatherCollection.find({}, function (err, records) { //find all records
-  res.render('data', {title:"Weather Data", weather: records });//render all records on albums/index
-});
+    weatherCollection.find({}, function(err, records) { //find all records
+        res.render('data', {
+            title: "Weather Data",
+            weather: records
+        }); //render all records on albums/index
+    });
 });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
 /* GET about page. */
-router.get("/about", function (req,res,next) {
-  res.render("about", {title:'About'});
+router.get("/about", function(req, res, next) {
+    res.render("about", {
+        title: 'About'
+    });
 });
 /* GET contact page. */
-router.get("/contact", function (req, res, next) {
-  res.render("contact", {title:"Contact"});
+router.get("/contact", function(req, res, next) {
+    res.render("contact", {
+        title: "Contact"
+    });
 });
 /* GET data page. */
-router.get("/data", function (req, res, next) {
-  res.render("data", {title:"Contact"});
+router.get("/data", function(req, res, next) {
+    res.render("data", {
+        title: "Contact"
+    });
 });
 //get weather data
 router.post('/weather', function(req, res, next) {
-  var zip = req.body.zip;
-  // console.log(zip);
-  weather(zip);
-  res.render('index', {title: "Check LCD"});
+
+    var locals = timeInput(req.body);
+    var hour = locals[0];
+    var min = locals[1];
+    var zip = req.body.zip;
+    console.log(hour);
+
+
+    console.log(hour);
+    weather(zip, hour, min);
+
+    res.render('index', {
+        title: "Check Arduino LCD for weather data"
+    });
+
 });
 
 
-router.post('/contact' , function (req,res,next) {
-
-sendgrid.send({
-  to:       'weberl48@outlook.com',
-  from:     req.body.email.trim(),
-  subject:  req.body.subject,
-  text:     req.body.body
-}, function(err, json) {
-  if (err) { return console.error(err); }
-  console.log(json);
-});
-res.render('contact', {message:"Email Sent Thank You"});
+router.post('/contact', function(req, res, next) {
+    var locals = validate(req.body);
+    if (locals.errors.length > 0) {
+        res.render('contact', error);
+    } else {
+        sendgrid.send({
+            to: 'weberl48@outlook.com',
+            from: req.body.email.trim(),
+            subject: req.body.subject,
+            text: req.body.body
+        }, function(err, json) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log(json);
+        });
+        res.render('contact', {
+            message: "Email Sent Thank You"
+        });
+    }
 });
 
 
--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-module.exports = function(zip) {
+module.exports = function(zip, hour, min) {
     var express = require("express");
     var five = require("johnny-five");
     var request = require("request");
@@ -7,6 +7,8 @@ module.exports = function(zip) {
     var weatherCollection = db.get('weather');
     var temp = "F";
     var board = new five.Board();
+    hour = hour;
+    min = min;
     var rows = 2;
     var cols = 16;
     var lcd;
@@ -15,89 +17,174 @@ module.exports = function(zip) {
         tempK = (tempK - 273.15) * 1.8000 + 32.00;
         return parseInt(tempK);
     }
+
+
+
+
     //when board is on do this function
     board.on('ready', function() {
+        var state = 0;
+        console.log("Board Ready");
+        var button = new five.Button({
+
+            pin: 7,
+            holdtime: 2000,
+            invert: false
+        });
+        var lbutton = new five.Button({
+
+            pin: 8,
+            holdtime: 2000,
+            invert: false
+        });
+
+
+        var piezo = new five.Piezo(9);
+
+
+
+        console.log("Board Ready");
         lcd = new five.LCD({
             pins: [12, 11, 5, 4, 3, 2],
             rows: rows,
             cols: cols
         });
-        lcd.clear();
-        lcd.print("Generating Weather Report");
-        //api get pulling weather info based on zip code
-        request("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us", function(error, response, body) {
-            data = JSON.parse(body);
-            var city = data.name;
-            var temp = kToF(data.main.temp);
-            var minTemp = kToF(data.main.temp_min);
-            var maxTemp = kToF(data.main.temp_max);
-            var conditions = data.weather[0].description;
-            toScreen(city, temp, minTemp, maxTemp, conditions);
-        });
-    });
-    // weatherCollection.insert({forcast: response.body, date: respnse.body.date }); //inserts user form data into mongo db album-demo.albumCollection
-    //allows us to access functions within node
-    //sending request data to screen formated for output
-    function toScreen(city, temp, minTemp, maxTemp, conditions) {
-        //first Row
-        var degSymbol = [4, 10, 4, 0, 0, 0, 0]; //creating a custom degree symbol by assigning pixels
-        lcd.createChar('degree', degSymbol); //making character a useable to arduino
-        lcd.useChar('degree'); //telling arduino to use the character made
-        lcd.clear().print(city); //display city name
-        lcd.setCursor(cols - 4, 0).print(temp).setCursor(cols - 2, 0).print(":degree:" + "F");
-        //second Row
-        var secondLine = 0;
-        var messages = 3;
-        ///every 2 secs change second row display
-        board.loop(2000, function() {
-            lcd.setCursor(0, rows - 1).print("                ");
-            switch (secondLine) {
-                case 0:
-                    lcd.setCursor(0, rows - 1).print(conditions);
-                    break;
-                case 1:
-                    lcd.setCursor(0, rows - 1).print("High of " + maxTemp + ":degree:" + "F"); // Display high temperature
-                    break;
-                case 2:
-                    lcd.setCursor(0, rows - 1).print("Low of " + minTemp + ":degree:" + "F"); // Display low temperature
-                    break;
+        var rule = new schedule.RecurrenceRule();
+        rule.dayOfWeek = new schedule.Range(0, 6);
+
+        rule.hour = Number(hour);
+        console.log(min);
+        rule.minute = Number(min);
+
+        var j = schedule.scheduleJob(rule, function() {
+
+
+            alarm();
+
+            function alarm() {
+                piezo.play({
+                    song: [
+                        [698, 100], // Play frequency 698 for 1 beat
+                        // ...
+                    ]
+                });
+
+                button.on("press", function() {
+                    piezo.noTone(9);
+                    console.log('Alarm Off');
+                    getWeather();
+                });
+
+
             }
-            secondLine++;
-            if (secondLine > messages - 1) {
-                secondLine = 0;
+
+            function getWeather() {
+                console.log("Generating Weather Report");
+                lcd.clear();
+                lcd.print("Generating Weather Report");
+                //api get pulling weather info based on zip code
+                request("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us", function(error, response, body) {
+                    data = JSON.parse(body);
+                    var city = data.name;
+                    var temp = kToF(data.main.temp);
+                    var minTemp = kToF(data.main.temp_min);
+                    var maxTemp = kToF(data.main.temp_max);
+                    var conditions = data.weather[0].description;
+                    toScreen(city, temp, minTemp, maxTemp, conditions);
+
+                });
             }
         });
-        var button = new five.Button({
-                board: board,
-                pin: 7,
-                holdtime: 2000,
-                invert: false
-            }
-        );
-        board.repl.inject({
-            button: button
-        });
-        var lbutton = new five.Button(8);
-        lbutton.on("press", function() {
-            lcd.clear().print("Save Data?");
-        });
-        board.repl.inject({
-            lbutton: lbutton
-        });
-        button.on("press", function() {
-            weatherCollection.insert({
-                city: city,
-                temp: temp,
-                minTemp: minTemp,
-                maxTemp: maxTemp,
-                conditions: conditions,
-                date: Date()
+
+        //allows us to access functions within node
+        //sending request data to screen formated for output
+        function toScreen(city, temp, minTemp, maxTemp, conditions) {
+            // city=city;
+            // temp=temp;
+            //first Row
+            var degSymbol = [4, 10, 4, 0, 0, 0, 0]; //creating a custom degree symbol by assigning pixels
+            lcd.createChar('degree', degSymbol); //making character a useable to arduino
+            lcd.useChar('degree'); //telling arduino to use the character made
+            lcd.clear().print(city); //display city name
+            lcd.setCursor(cols - 4, 0).print(temp).setCursor(cols - 2, 0).print(":degree:" + "F");
+            //second Row
+            var secondLine = 0;
+            var messages = 3;
+            ///every 2 secs change second row display
+            board.loop(2000, function() {
+                lcd.setCursor(0, rows - 1).print("                ");
+                switch (secondLine) {
+                    case 0:
+                        lcd.setCursor(0, rows - 1).print(conditions);
+                        break;
+                    case 1:
+                        lcd.setCursor(0, rows - 1).print("High of " + maxTemp + ":degree:" + "F"); // Display high temperature
+                        break;
+                    case 2:
+                        lcd.setCursor(0, rows - 1).print("Low of " + minTemp + ":degree:" + "F"); // Display low temperature
+                        break;
+                }
+                secondLine++;
+                if (secondLine > messages - 1) {
+                    secondLine = 0;
+                }
+
             });
-            lcd.clear().print("Saved");
-        });
-        button.on("hold", function() {
-            lcd.clear();
-            lcd.print(city);
-        });
-    }
+
+
+            buttons(city, temp, minTemp, maxTemp, conditions);
+        }
+
+
+
+
+        function buttons(city, temp, minTemp, maxTemp, conditions) {
+
+
+            console.log(city);
+
+            board.repl.inject({
+                button: button
+            });
+
+            lbutton.on("press", function() {
+                lcd.clear().print("Save Data?");
+            });
+            lbutton.on("hold", function() {
+                weatherCollection.insert({
+                    city: city,
+                    temp: temp,
+                    minTemp: minTemp,
+                    maxTemp: maxTemp,
+                    conditions: conditions,
+                    date: Date()
+                });
+                lcd.clear().print("Saved");
+                lcd.clear();
+                lcd.print(city);
+                lcd.setCursor(cols - 4, 0).print(temp).setCursor(cols - 2, 0).print(":degree:" + "F");
+            });
+            board.repl.inject({
+                lbutton: lbutton
+            });
+            button.on("press", function() {
+                weatherCollection.insert({
+                    city: city,
+                    temp: temp,
+                    minTemp: minTemp,
+                    maxTemp: maxTemp,
+                    conditions: conditions,
+                    date: Date()
+                });
+                lcd.clear().print("Saved");
+            });
+            button.on("hold", function() {
+                lcd.clear();
+                lcd.print(city);
+                lcd.setCursor(cols - 4, 0).print(temp).setCursor(cols - 2, 0).print(":degree:" + "F");
+            });
+        }
+
+    });
+
 };
